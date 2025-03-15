db9ef8873a7288c4e604b6f3c7ec37484031ed6b
Bashyr-Ali <bashyrali@mail.ru>
2025-03-15 20:38:40 +0500

    add Minimal API with post method

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,8 +1,15 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
     //Add services in container
-
+    builder.Services.AddMediatR(config =>
+    {
+        config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    });
+    builder.Services.AddCarter();
 var app = builder.Build();
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 
 app.Run();
