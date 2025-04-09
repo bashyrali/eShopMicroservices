11eec78b3a2b43ee7e6691c74c95027aa11e6aed
Bashyr-Ali <bashyrali@mail.ru>
2025-04-09 22:30:45 +0500

    Add DeleteProductHandler.cs and DeleteProductEndpoint.cs

---
+++ src/Services/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,21 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        session.Delete<Product>(request.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
